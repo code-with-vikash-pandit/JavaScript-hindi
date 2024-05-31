f93b98fd9ecf8f54fab4f22f15894eaaeb306b54
er vikash <razavikash1234@gmail.com>
2024-05-31 13:19:09 +0530

    Use in dataTypes

---
+++ 02_dataTypes.js
@@ -0,0 +1,28 @@
+"Use Strict"; // treat aal JS code as newer version.
+
+// alert("Hello Vikash") we are using NodeJs, Not Browser
+
+console.log(5+
+    6); // code redability should be high.
+
+console.log("Vikash");
+
+let name =("Vikash")
+let age= 22
+let isLoggedIn =false
+
+/* DataTypes
+ number => 2 to power 53.
+ string => ""
+ boolean => true or false
+ null => empty value
+ undefined => not assigned value
+ symbol => unique 
+
+ object 
+*/
+
+console.log(typeof undefined); // example- undefined hi hota hai.
+
+console.log(typeof null); // example- Object hi hota hai.
+
