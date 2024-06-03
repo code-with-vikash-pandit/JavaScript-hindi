b714c76447ec0414b6f7046525eaade5fe79692d
Learn Loading <142532080+code-with-vikash-pandit@users.noreply.github.com>
2024-06-03 11:27:22 +0000

    conversion

---
+++ 1_Basic/4_comparision.js
@@ -0,0 +1,16 @@
+// console.log(2> 1);
+// console.log(2>= 1);
+// console.log(2<1);
+// console.log(2== 1);
+// console.log(2!=1);
+
+// console.log("10" <5);
+// console.log("3" >1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+
+console.log("3" === 2);
