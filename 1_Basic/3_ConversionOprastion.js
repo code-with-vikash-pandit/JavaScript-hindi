946b30bbefc1e6e57c52327e3ee10790dc3139f4
Learn Loading <142532080+code-with-vikash-pandit@users.noreply.github.com>
2024-06-03 11:02:11 +0000

    add oprastion

---
+++ 1_Basic/3_ConversionOprastion.js
@@ -0,0 +1,72 @@
+let score = "Vikash"
+
+//console.log (typeof score);
+//console.log (typeof(score));
+
+// let valueInNumber = Number (score)
+
+//console.log (typeof valueInNumber);
+//console.log (valueInNumber);
+
+
+// "33" => 33
+// "33abcd" => NaN
+// true => 1; false => 0
+
+// let isLoggedIn = "Kumar"
+
+// let booleanIsLoggedIn = Boolean(IsLoggedIn)
+// console.log (booleanIsLoggedIn);
+
+// 1 => true; 0=> false
+// "" => true
+// "kumar" => true
+
+// let stringNumber = string(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ****************** Oprations************
+
+let value = 3
+let negValue = -value 
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = "Vikash"
+ let str3 = str1 + str2
+//  console.log(str3);
+
+//  console.log("1" + 2);
+//  console.log(1+ "2");
+//  console.log("1" + 2 + 2);
+//  console.log(1 + 2 + "2");
+
+//  console.log(+true);
+//  console.log(+ "");
+
+let num, num2, num3
+
+num1= num= num= 2+2
+
+let gameCounter = 100
+gameCounter++;
+// console.log(gameCounter);
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
