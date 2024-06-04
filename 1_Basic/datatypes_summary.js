c4e63c271ad754ea2666d594a3be0341a00d7f94
Learn Loading <142532080+code-with-vikash-pandit@users.noreply.github.com>
2024-06-04 06:31:25 +0000

    datatypes_sumary

---
+++ 1_Basic/datatypes_summary.js
@@ -0,0 +1,36 @@
+// Primitive
+
+// there are 7 types of primitive
+// 7:- String, Number, Boolean, null,           undefined, Symbol, BigInt.
+
+//  const score = 100
+//  const scroeValue = 100.4
+
+//  const isLoggedIn = false
+ const outsideTemp = unll
+//  let userEmail; //undefined
+
+//  const id = Symbol('1234')
+//  const anotherId = symbol ('1234')
+
+//  console.log(id === anotherId);
+
+// const bigNumber = 23410345625789n
+
+
+
+ // Reference (Non Primitive)
+
+ // Array, Objects, Function.
+
+ const heros = ["Vikash", "Mantu", "Sanjeev"];
+ let obj = {
+    name: "Prajapati",
+    age: 22,
+ }
+
+ const myFunction = function(){
+    console.log("hello Vikash");
+ }
+
+ console.log(typeof outsideTemp);
