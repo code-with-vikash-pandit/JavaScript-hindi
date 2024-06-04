0e0e7ee8bbbc6f69b2a3beb176401af1c9195efa
Learn Loading <142532080+code-with-vikash-pandit@users.noreply.github.com>
2024-06-04 11:31:23 +0000

    add to stack & Heap

--- 1_Basic/datatypes_summary.js
+++ 1_Basic/datatypes_summary.js
@@ -7,7 +7,7 @@
 //  const scroeValue = 100.4
 
 //  const isLoggedIn = false
- const outsideTemp = unll
+//  const outsideTemp = unll
 //  let userEmail; //undefined
 
 //  const id = Symbol('1234')
@@ -23,14 +23,41 @@
 
  // Array, Objects, Function.
 
- const heros = ["Vikash", "Mantu", "Sanjeev"];
- let obj = {
-    name: "Prajapati",
-    age: 22,
- }
+//  const heros = ["Vikash", "Mantu", "Sanjeev"];
+//  let obj = {
+//     name: "Prajapati",
+//     age: 22,
+//  }
 
- const myFunction = function(){
-    console.log("hello Vikash");
- }
+//  const myFunction = function(){
+//     console.log("hello Vikash");
+//  }
 
- console.log(typeof outsideTemp);
+//  console.log(typeof outsideTemp);
+
+
+
+ // *************Memory*********************
+//  there are teo types of memory
+// 1:- Stack (Primitive)- Value decision aata hai, o value copy milta h.
+
+// 2:- Heap (Non-Primitive)- value ka  refirece milta hai. 
+
+let myVillageName = Doila
+
+let anotherDist = siwan
+anotherDist = "patna"
+
+console.log(myVillageName);
+
+let userOne = {
+    email: "user@outlook.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "vikash@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
