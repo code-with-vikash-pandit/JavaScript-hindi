440b912feeca7941dccdb5aff6859c1d19934647
Learn Loading <142532080+code-with-vikash-pandit@users.noreply.github.com>
2024-06-07 18:18:17 +0000

    Num and Maths

---
+++ 1_Basic/6_nums_and_maths.js
@@ -0,0 +1,35 @@
+const score = 500
+
+// console.log(score);
+
+const amount = new Number (1500)
+// console.log(amount);
+// console.log(amount.toString());
+// console.log(amount.toLocaleString().length);
+
+// console.log(amount.toFixed(1));
+
+const otherNumber = 124.985
+// console.log(otherNumber.toPrecision(4)); // round number 
+
+const hundreds = 200000
+//console.log(hundreds.toLocaleString('en-IN')); // conver to india peisaa ex-'2,00,000'
+
+// ********************Math's*****************************
+
+// console.log(Math);
+// console.log(Math.abs(-9)); //  nigetive value ko positive karta hai. only
+// console.log(Math.round(51.245)); // round figure me jo use ex- o ekject value detha hai.
+// console.log(Math.ceil(10.5)); // ceil yaa agar ex- 10.5 ho to hum use, 11 kar denge. Only UP value.
+// console.log(Math.floor(12.9)); // floor yaa agar ex- 12.9 ho to hum use, 12 kar denge, only DOWN value.
+// console.log(Math.min(9,8,3,2,1,0)); // sab number me se LOW Value result dega.only
+// console.log(Math.max(90,50,80,70,)); // sab number me se HIGH Value result dega.only
+
+console.log(Math.random());
+console.log(MAth.floor(Math.random()*10)+1);
+
+const min = 20
+const max = 40
+
+console.log(Math.floor.random()*(max - min + 1)) + min
+
