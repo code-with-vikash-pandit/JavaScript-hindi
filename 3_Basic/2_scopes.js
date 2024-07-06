245010ad1cad2c2aea466761553779a913fa1376
Learn Loading <142532080+code-with-vikash-pandit@users.noreply.github.com>
2024-07-06 10:32:52 +0000

    scopes

---
+++ 3_Basic/2_scopes.js
@@ -0,0 +1,25 @@
+// SCOPES // 
+
+let a = 400
+if (true) {
+    let a = 40
+    const b = 60 
+}
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one (){
+    const username = "Vikash"
+
+    function two() {
+        const website = "youtube"       
+    }
+    // console.log(website)
+
+    two()
+}
+
+// one()
