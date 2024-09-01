6e8ebd9121fd1772caf538776167f4acc420f04e
Learn Loading <142532080+code-with-vikash-pandit@users.noreply.github.com>
2024-09-01 02:12:32 +0000

    Switch Statement

---
+++ 3_Basic/04_switch.js
@@ -0,0 +1,48 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month =3 // Number match case
+
+// switch (month) {
+//     case 1:
+//         console.log("january");
+//         break;
+//     case 2:
+//         console.log("february");
+//         break;
+//     case 3:
+//         console.log("march");
+//         break;
+//     case 4:
+//         console.log("April");
+//         break;
+//     default:
+//         console.log("defaul cese match");
+//         break;
+// }
+
+const month = "March"  // String case match
+
+switch (month) { // Control flow Statement 
+    case "Jan":
+        console.log("january");
+        break;
+    case "feb":
+        console.log("february");
+        break;
+    case "March":
+        console.log("march");
+        break;
+    case "Apr":
+        console.log("April");
+        break;
+    default:
+        console.log("defaul cese match");
+        break;
+}
