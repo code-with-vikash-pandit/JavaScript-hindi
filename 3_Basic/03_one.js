da730470ed1af434c313342a9aed9ace09d6088d
Learn Loading <142532080+code-with-vikash-pandit@users.noreply.github.com>
2024-09-01 01:38:57 +0000

    Shoping use code & Loging

---
+++ 3_Basic/03_one.js
@@ -0,0 +1,52 @@
+// const isUserloggedIn = true
+// const temperature = 43
+
+// if (temperature > 43 ) {
+//     console.log("temperature is Low 43"); 
+// }else{
+//     console.log ("temperature is more them high");
+// }
+
+// console.log("exuction");
+
+// const score = 300
+
+// if (score >170){
+//     let power = "fly"
+//     console.log (`user power: ${power}`);
+// }
+
+// console.log(`user power: ${power}`);
+
+// const balance = 2500
+
+// if (balance >1700)console.log("test"), console.log("test2");
+    
+// if (balance <700) {
+//     console.log("less than");
+    
+// }else if (balance < 850){
+//     console.log("less than 850");
+    
+// }else if (balance <950) {
+//     console.log("less than 650");
+    
+// }else {
+//     console.log("less than 1350");
+    
+// }
+
+// Shoping karne ke liye use kiya jaata hai.
+const userLoggedIn = true
+const debitcard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitcard && 2==2) {  // Dono chek karta hai (OUR Statement Hai)
+    console.log("Allow to buy course");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {  // Dono Multipal Condition check to all.
+    console.log("User logged in");
+    
+}
